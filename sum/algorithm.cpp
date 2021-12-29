ae1ebe6875dd5e5c912a5fe85bb195c3bd7fe6aa
Melján Dávid <51965039+meljandavid@users.noreply.github.com>
2021-12-29 09:29:01 +0100

    Update algorithm.cpp

--- sum/algorithm.cpp
+++ sum/algorithm.cpp
@@ -6,10 +6,10 @@ struct Point { int x,y; };
 std::list<Point> generateMap(int N) {
     std::list<Point> buildings;
     int columns = int(std::log2(N-1)) + 1;
-    int upperpow = (1 << (columns+1));
+    int upperpow = (1 << (columns));
 
     for (int c = 0; c < columns; c++) {
-        for (int y = (1<<c); y <= upperpow; y++) {
+        for (int y = (1<<c); y <= 2*upperpow; y++) {
             if ( ( (y - (1 << c) ) % (1 << (c + 2) ) ) < (1 << (c + 1) ) ) {
                 buildings.push_back({ 2 * c,y });
                 if (c != 0) buildings.push_back({ -2 * c,y });
@@ -17,7 +17,7 @@ std::list<Point> generateMap(int N) {
         }
     }
 
-    int deletable = upperpow/2 - N;
+    int deletable = upperpow - N;
     for (int c = columns - 1; c >= 0; c--) {
         if (deletable >= (1 << c)) {
             deletable -= (1 << c);
